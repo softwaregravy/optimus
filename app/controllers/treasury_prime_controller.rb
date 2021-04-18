26f375cf38b4a19fb8de70e553a7ebc73053bde6
softwaregravy <426690+softwaregravy@users.noreply.github.com>
2021-04-18 09:59:06 -0400

    list_accounts

--- app/controllers/treasury_prime_controller.rb
+++ app/controllers/treasury_prime_controller.rb
@@ -1,17 +1,23 @@
 class TreasuryPrimeController < ApplicationController
   def ping
+    @controller_method = "TreasuryPrime#ping"
     service = TreasuryPrime.new
     @uri_string, @response = service.ping
-    @body_values = {}
-    begin
-      puts JSON.parse(@response.body)
-
-      @body_values = JSON.parse(@response.body)
-      puts "body values: #{@body_values}"
-    rescue nil
-    end
+    @body_values = parse_body(@response)
   end
 
-  def authed_ping
+  def accounts
+    @controller_method = "TreasuryPrime#accounts"
+    service = TreasuryPrime.new
+    @uri_string, @response = service.account
+    @body_values = parse_body(@response)
+    render :ping
+  end
+
+  private
+  def parse_body(tp_response)
+    body_values = {}
+    body_values = JSON.parse(tp_response.body) rescue nil
+    body_values
   end
 end
--- app/services/treasury_prime.rb
+++ app/services/treasury_prime.rb
@@ -14,4 +14,18 @@ class TreasuryPrime
     return @uri_string, @response
   end
 
+  def account
+    uri = URI::HTTPS.build(
+      host: TREASURY_PRIME_ENDPOINT, 
+      path: '/account')
+    @uri_string = uri.to_s
+    connection = Faraday.new(url: @uri_string) do |conn|
+      conn.basic_auth(Rails.application.credentials.treasury_direct_api_key, Rails.application.credentials.treasury_direct_secret_key)
+    end
+    @response = connection.get
+
+
+    return @uri_string, @response
+  end
+
 end
